f57f857a049bdd37e3368c713dedd65ce1506249
ashish.dsouza <ashish.dsouza@students.polytech.k12.de.us>
2018-11-02 13:39:11 -0400

    Update

--- python/dnn.py
+++ python/dnn.py
@@ -5,12 +5,53 @@
 import numpy as np
 import tensorflow as tf
 
-
 categorical_feature_a = categorical_column_with_hash_bucket(...)
 categorical_feature_b = categorical_column_with_hash_bucket(...)
 
-categorical_feature_a_emb = embedding_column(categorical_column=categorical_feature_a, ...)
-categorical_feature_b_emb = embedding_column(categorical_column=categorical_feature_b, ...)
+categorical_feature_a_emb = embedding_column(
+    categorical_column=categorical_feature_a, ...)
+categorical_feature_b_emb = embedding_column(
+    categorical_column=categorical_feature_b, ...)
 
-estimator = tf.estimator.DNNRegressor(feature_columns=[categorical_feature_a_emb, categorical_feature_b_emb], hidden_units=[1024, 512, 256])
+estimator = DNNRegressor(
+    feature_columns=[categorical_feature_a_emb, categorical_feature_b_emb],
+    hidden_units=[1024, 512, 256])
 
+# Or estimator using the ProximalAdagradOptimizer optimizer with
+# regularization.
+estimator = DNNRegressor(
+    feature_columns=[categorical_feature_a_emb, categorical_feature_b_emb],
+    hidden_units=[1024, 512, 256],
+    optimizer=tf.train.ProximalAdagradOptimizer(
+      learning_rate=0.1,
+      l1_regularization_strength=0.001
+    ))
+
+# Or estimator using an optimizer with a learning rate decay.
+estimator = DNNRegressor(
+    feature_columns=[categorical_feature_a_emb, categorical_feature_b_emb],
+    hidden_units=[1024, 512, 256],
+    optimizer=lambda: tf.AdamOptimizer(
+        learning_rate=tf.exponential_decay(
+            learning_rate=0.1,
+            global_step=tf.get_global_step(),
+            decay_steps=10000,
+            decay_rate=0.96))
+
+# Or estimator with warm-starting from a previous checkpoint.
+estimator = DNNRegressor(
+    feature_columns=[categorical_feature_a_emb, categorical_feature_b_emb],
+    hidden_units=[1024, 512, 256],
+    warm_start_from="/path/to/checkpoint/dir")
+
+# Input builders
+def input_fn_train: # returns x, y
+  pass
+estimator.train(input_fn=input_fn_train, steps=100)
+
+def input_fn_eval: # returns x, y
+  pass
+metrics = estimator.evaluate(input_fn=input_fn_eval, steps=10)
+def input_fn_predict: # returns x, None
+  pass
+predictions = estimator.predict(input_fn=input_fn_predict)
