3ccacb9d890fc80917c6b6e6dc8216c1cbcb937c
ashish.dsouza <ashish.dsouza@students.polytech.k12.de.us>
2018-11-08 14:53:30 -0500

    Update

--- python/Sandbox.py
+++ python/Sandbox.py
@@ -1,11 +1,18 @@
 # Sandbox.py
 # Ashish D'Souza
-# October 29th, 2018
+# November 8th, 2018
 
-import numpy as np
-import tensorflow as tf
+import pandas as pd
+from sodapy import Socrata
+import os
 
-x_data = []
-y_data = []
 
-estimator = DNNRegressor()
+client = Socrata("data.delaware.gov", os.environ["soda_token"])
+results = client.get("2bb6-s69t", where="date_time=\"2015-10-28T20:00:00.000\" and countycode=3")
+results_df = pd.DataFrame.from_records(results)
+print(type(results))
+print(type(results_df))
+print(results)
+print(results_df)
+print(len(results))
+print(results[0])
