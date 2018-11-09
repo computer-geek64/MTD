4171fb51de46f8ee291d1e45d93831f003ae6331
ashish.dsouza <ashish.dsouza@students.polytech.k12.de.us>
2018-11-09 13:40:33 -0500

    Update

--- python/Sandbox.py
+++ python/Sandbox.py
@@ -8,7 +8,7 @@ import os
 
 
 client = Socrata("data.delaware.gov", os.environ["soda_token"])
-results = client.get("2bb6-s69t", where="date_time=\"2015-10-28T20:00:00.000\" AND countycode=1 AND sta_stationname=\"Martin Luther King\"")
+results = client.get("2bb6-s69t", where="date_time=\"2015-10-28T20:00:00.000\" AND countycode=3 AND sta_stationname=\"Martin Luther King\"")
 results_df = pd.DataFrame.from_records(results)
 print(type(results))
 print(type(results_df))
