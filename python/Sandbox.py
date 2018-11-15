6faac62330ddb71528de1d7177764bf7bf886533
computer-geek64 <ashish.dsouza@students.polytech.k12.de.us>
2018-11-15 01:59:59 -0500

    Update

--- python/Sandbox.py
+++ python/Sandbox.py
@@ -1,11 +1,3 @@
 # Sandbox.py
 # Ashish D'Souza
-# November 14th, 2018
-
-import autocomplete
-import gtts
-import os
-
-
-gtts.gTTS("Good morning", lang="en").save("speak.mp3")
-os.system("mpg123 speak.mp3; rm speak.mp3")
+# November 15th, 2018
