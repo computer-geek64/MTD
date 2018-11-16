7844f65dbbec1249b30a388cc15d0bcaee7124ea
ashish.dsouza <ashish.dsouza@students.polytech.k12.de.us>
2018-11-16 11:29:58 -0500

    Update

--- python/AnomalyDetection.py
+++ python/AnomalyDetection.py
@@ -4,3 +4,20 @@
 
 import numpy as np
 import tensorflow as tf
+
+
+def get_outliers(data, elimination_criteria, vector_index=0):
+    if len(np.shape(data)) == 1:
+        data = np.array([data])
+    return [observation for observation in data[vector_index] if not elimination_criteria(observation, data[vector_index])]
+
+
+def remove_outliers(data, elimination_criteria, vector_index=0):
+    if len(np.shape("data")) == 1:
+        data = np.array([data])
+    return [observation for observation in data[vector_index] if elimination_criteria(observation, data[vector_index])]
+
+
+def k_nearest_neighbor(data, k, vector_index=0):
+    if len(np.shape(data)) == 1:
+        data = np.array([data])
