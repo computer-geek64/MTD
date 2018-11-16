933a2551156926565ec6a04613082f708b801cde
computer-geek64 <ashish.dsouza@students.polytech.k12.de.us>
2018-11-15 23:01:24 -0500

    Update

--- python/Main.py
+++ python/Main.py
@@ -6,10 +6,6 @@ import os
 import Data as data
 
 
-def criteria(observation, dataset):
-    return data.median(dataset) - 1.5 * data.iqr(dataset) <= observation <= data.median(dataset) + 1.5 * data.iqr(dataset)
-
-
 soda = data.SODA("data.delaware.gov", "2bb6-s69t")
 print(soda.get_columns())
 where_query = soda.format_where_query(["countycode=\"3\"", "NOT stt_datastatuscodetext=\"Down\"", "NOT stt_datastatuscodetext=\"NoData\"", "NOT stt_datastatuscodetext=\"InVld\"", "mot_monitorname=\"Ozone\"", "sta_stationname=\"Martin Luther King\""])
@@ -18,6 +14,6 @@ results = [float(x["paramvalue"]) for x in results]
 print("Dataset length: " + str(len(results)))
 print(results)
 
-dataset = data.remove_outliers([results], criteria)
+dataset = data.remove_outliers([results], lambda observation, data_set: data.median(data_set) - 1.5 * data.iqr(data_set) <= observation <= data.median(data_set) + 1.5 * data.iqr(data_set))
 print(dataset)
 print(len(dataset))
