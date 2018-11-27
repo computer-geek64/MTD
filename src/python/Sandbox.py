0bab548749bf375f2f1ac21391be1bede5342751
computer-geek64 <ashish.dsouza@students.polytech.k12.de.us>
2018-11-26 19:12:58 -0500

    Update

--- src/python/Sandbox.py
+++ src/python/Sandbox.py
@@ -1,31 +1,50 @@
 # Sandbox.py
 # Ashish D'Souza
-# November 19th, 2018
+# November 26th, 2018
 
 import tensorflow as tf
 import numpy as np
+import Data
 
 
-k = 2
+k_percentage = 0.01
 data = [
-    [0, 3, 5, 15],
-    [1, 6, 4, 17],
-    [2, 9, 3, 19],
-    [3, 12, 2, 21],
-    [4, 15, 1, 23]
+    [2, 5],
+    [8, 9],
+    [1, 3],
+    [4, 4],
+    [9, 6],
+    [11, 13],
+    [5, 10],
+    [8, 8],
+    [9, 15],
+    [1, 9],
+    [1, 19]
 ]
 
 training_data = tf.placeholder(dtype=tf.float32)
 testing_data = tf.placeholder(dtype=tf.float32)
+distance_placeholder = tf.placeholder(dtype=tf.float32)
 
-distances = tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(training_data, testing_data)), axis=1))
+k = int(k_percentage * len(data)) if len(data) >= 300 else 3
+
+squared_differences = tf.square(tf.subtract(training_data, testing_data))
+euclidean_distances = tf.sqrt(tf.reduce_sum(squared_differences, axis=1))
+
+knn_distances = tf.negative(tf.nn.top_k(tf.negative(distance_placeholder), k + 1).values)[1:]
+
+knn_distance_mean = tf.reduce_mean(knn_distances)
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
 
 sess.run(init)
+
+knn_distance_means = []
 for observation in range(len(data)):
-    distance = sess.run(distances, feed_dict={training_data: data, testing_data: data[observation]})
-    print(distance)
-    lowest_k_distances = sess.run(tf.nn.top_k(tf.negative(distance), k + 1))[1][1:]
-    print(lowest_k_distances)
+    distance = sess.run(euclidean_distances, feed_dict={training_data: data, testing_data: data[observation]})
+    knn_distance_means.append(sess.run(knn_distance_mean, feed_dict={distance_placeholder: distance}))
+print(Data.iqr(knn_distance_means) * 1.5)
+print(Data.median(knn_distance_means))
+print(knn_distance_means)
+print([x for x in knn_distance_means if x < Data.iqr(knn_distance_means) * 1.5 + Data.median(knn_distance_means)])
