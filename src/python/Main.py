07ccca954d60b26eac6b488b0c30d8935ff56cd1
computer-geek64 <ashish.dsouza@students.polytech.k12.de.us>
2018-11-27 23:40:53 -0500

    Update

--- src/python/Main.py
+++ src/python/Main.py
@@ -10,21 +10,23 @@ import OutlierDetection
 soda = Data.SODA("data.delaware.gov", "2bb6-s69t")
 print(soda.get_columns())
 where_query = soda.format_where_query(["countycode=\"3\"", "NOT stt_datastatuscodetext=\"Down\"", "NOT stt_datastatuscodetext=\"NoData\"", "NOT stt_datastatuscodetext=\"InVld\"", "sta_stationname=\"Martin Luther King\""])
-results = soda.download(where=where_query, order="date_time DESC", limit=1000)
+results = soda.download(where=where_query, order="date_time DESC", limit=1945)
 print("Dataset length: " + str(len(results)))
-print(results)
 
-date_time_list = []
+data_dict = {}
 for observation in range(len(results)):
-    if results[observation]["date_time"] not in monitor_names:
-        date_time_list.append(results[observation]["date_time"])
+    if results[observation]["date_time"] not in data_dict.keys():
+        data_dict[results[observation]["date_time"]] = {}
+    data_dict[results[observation]["date_time"]][results[observation]["mot_monitorname"]] = float(results[observation]["paramvalue"])
 
+data = []
+for date_time in sorted(data_dict.keys()):
+    data.append([])
+    for monitor_name in sorted(data_dict[date_time].keys()):
+        data[-1].append(data_dict[date_time][monitor_name])
+if len(data[0]) != len(data[1]):
+    data.pop(0)
 
-
-monitor_names = []
-for observation in range(len(results)):
-    if results[observation]["mot_monitorname"] not in monitor_names:
-        monitor_names.append(results[observation]["mot_monitorname"])
-variables = [results[x]["mot_monitorname"] for x in range(len(results))]
-values = [results[x]["paramvalue"] for x in range(len(results))]
-data = [[results[observation]] for observation in range(len(results))]
+outliers = OutlierDetection.knn_outlier_detection(data)
+print(len(data))
+print(outliers)
