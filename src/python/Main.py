83e711d780eaf30c883868518d77f48c9cc759a2
ashish.dsouza <ashish.dsouza@students.polytech.k12.de.us>
2018-11-30 14:52:08 -0500

    Update

--- src/python/Main.py
+++ src/python/Main.py
@@ -7,45 +7,43 @@ import Data
 import OutlierDetection
 
 
+parameters = ["Temp", "NO2", "NOX", "NOY", "Ozone", "RH", "Wind Speed V"]
+
 soda = Data.SODA("data.delaware.gov", "2bb6-s69t")
 print(soda.get_columns())
 where_query = soda.format_where_query(["countycode=\"3\"", "NOT stt_datastatuscodetext=\"Down\"", "NOT stt_datastatuscodetext=\"NoData\"", "NOT stt_datastatuscodetext=\"InVld\"", "sta_stationname=\"Martin Luther King\""])
 results = soda.download(where=where_query, order="date_time DESC", limit=100000)
-print("Dataset length: " + str(len(results)))
 
 data_dict = {}
 for observation in range(len(results)):
     if results[observation]["date_time"] not in data_dict.keys():
         data_dict[results[observation]["date_time"]] = {}
     data_dict[results[observation]["date_time"]][results[observation]["mot_monitorname"]] = float(results[observation]["paramvalue"])
-parameters = ["8 HR Ozone", "AQS Temp Deg C", "BC 880", "CO Trace Level", "NO2", "NOX", "NOY", "Ozone", "PM 2.5 TAPI", "PM2.5 5030i IN", "RH", "SO2 Trace Level", "SO2-Max5min", "Temp", "Wind Dir V", "Wind Speed V"]
-default_length = max(map(len, data_dict.values()))
+all_parameters = []
+for observation in range(len(results)):
+    if results[observation]["mot_monitorname"] not in all_parameters:
+        all_parameters.append(results[observation]["mot_monitorname"])
+all_parameters = sorted(all_parameters)
 original_length = len(data_dict)
-print("Default length: " + str(default_length))
 
 data = []
 for date_time in sorted(data_dict.keys()):
     data.append([])
-    if sorted(list(data_dict[date_time].keys())) == parameters:
-        for monitor_name in sorted(data_dict[date_time].keys()):
-            data[-1].append(data_dict[date_time][monitor_name])
-    elif len(data_dict[date_time].keys())
-    if len(data_dict[date_time]) != default_length:
+    if len([parameters[i] for i in range(len(parameters)) if parameters[i] in list(data_dict[date_time].keys())]) == len(parameters):
+        data[-1] = [data_dict[date_time][monitor_name] for monitor_name in parameters]
+    else:
         data.pop(-1)
         data_dict.pop(date_time)
 
-print(sorted(list(data_dict[list(data_dict.keys())[33]].keys())))
 if len(max(data)) != len(min(data)):
     print("Length of data matrix is inconsistent")
     exit(0)
 
-print("Dataset size: " + str(original_length) + " --> " + str(len(data)))
-
-k = int(round(len(data) * 0.001))
+# k = int(round(len(data) * 0.0001))
+k = 3
 outliers = OutlierDetection.knn_outlier_detection(data, k=k)
+
+print("All parameters: " + str(all_parameters))
+print("Dataset size: " + str(len(results)) + " --> " + str(original_length) + " --> " + str(len(data)))
 print("k: " + str(k))
-print(outliers)
-print(len(outliers))
-print(str(round(len(outliers) / len(data) * 100, 2)) + "%")
-print(list(data_dict.keys())[0])
-print(list(data_dict.keys())[-1])
+print("Outliers: " + str(len(outliers)) + ", " + str(round(len(outliers) / len(data) * 100, 2)) + "%")
